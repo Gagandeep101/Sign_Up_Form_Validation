3b419ba3abc274aac3db2569278a3486c934aaa4
Gagandeep_Singh_101 <ts026925@gmail.com>
2023-05-18 13:57:42 +0530

    feat : add click event listener on Sign_Up_btn
    After adding click event listener on Sign_Up_btn . We adding submit event
    Listener on form_data . And in it used validate_form_data function

--- brain.js
+++ brain.js
@@ -1,2 +1,10 @@
 var form_data = document.querySelector(".form");
 var Sign_Up_btn = document.querySelector("#Sign_Up_btn");
+Sign_Up_btn.addEventListener("click", function () {
+    form_data.addEventListener("submit", (e) => {
+        e.preventDefault();
+        validate_form_data();
+    })
+
+})
+
