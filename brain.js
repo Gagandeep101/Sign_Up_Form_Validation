6edea78e9c91c68d11b26bb03ec15b7aa4ee80f2
Gagandeep_Singh_101 <ts026925@gmail.com>
2023-05-18 14:04:52 +0530

    refactor : declare validate_form_data fucntion
    import all input tag with its predefined id attr . And then check if all tags is empty or not .If it is empty show alert message otherwise use validate_user_name function , validate_user_phone_number function, validate_user_password function

--- brain.js
+++ brain.js
@@ -8,3 +8,19 @@ Sign_Up_btn.addEventListener("click", function () {
 
 })
 
+function validate_form_data() {
+    const first_name = document.getElementById("user_first_name");
+    const last_name = document.getElementById("user_last_name");
+    const phone_number = document.getElementById("user_phone_number");
+    const password = document.getElementById("user_password");
+    const confirm_password = document.getElementById("user_confirm_password");
+    if (first_name.value === "" || last_name.value === "" || phone_number.value === "" || password.value === "" || confirm_password.value === "") {
+        alert("All fields are required");
+    } else {
+        validate_user_name(first_name);
+        validate_user_name(last_name);
+        validate_user_phone_number(phone_number);
+        validate_user_password(password, confirm_password);
+    }
+}
+
